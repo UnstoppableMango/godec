5944fd72400d7afd1832d4589c28fe0d1d42cab7
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-05-19 23:34:21 -0500

    WIP

--- internal/cast.go
+++ internal/cast.go
@@ -6,50 +6,42 @@ import (
 	"github.com/unmango/go/codec"
 )
 
-type castd[T any] struct {
+type Decoder[T any] struct {
 	codec.Decoder[any]
 }
 
-func (d castd[T]) Decode(v T) error {
+func (d Decoder[T]) Decode(v T) error {
 	return d.Decoder.Decode(&v)
 }
 
-func AnyD[T any](d codec.Decoder[any]) codec.Decoder[T] {
-	return castd[T]{d}
-}
-
-type caste[T any] struct {
+type Encoder[T any] struct {
 	codec.Encoder[any]
 }
 
-func (e caste[T]) Encode(v T) error {
+func (e Encoder[T]) Encode(v T) error {
 	return e.Encoder.Encode(v)
 }
 
-func AnyE[T any](e codec.Encoder[any]) codec.Encoder[T] {
-	return caste[T]{e}
-}
-
-type castc[T any, V any] struct {
+type cast[T any, V any] struct {
 	codec codec.Codec[V]
 }
 
-func (c castc[T, V]) Marshal(v T) ([]byte, error) {
+func (c cast[T, V]) Marshal(v T) ([]byte, error) {
 	return c.codec.Marshal(any(v).(V))
 }
 
-func (c castc[T, V]) NewDecoder(r io.Reader) codec.Decoder[T] {
+func (c cast[T, V]) NewDecoder(r io.Reader) codec.Decoder[T] {
 	return c.codec.NewDecoder(r).(codec.Decoder[T])
 }
 
-func (c castc[T, V]) NewEncoder(w io.Writer) codec.Encoder[T] {
+func (c cast[T, V]) NewEncoder(w io.Writer) codec.Encoder[T] {
 	return c.codec.NewEncoder(w).(codec.Encoder[T])
 }
 
-func (c castc[T, V]) Unmarshal(data []byte, v T) error {
+func (c cast[T, V]) Unmarshal(data []byte, v T) error {
 	return c.codec.Unmarshal(data, any(v).(V))
 }
 
 func Cast[T any, V any, C codec.Codec[V]](c C) codec.Codec[T] {
-	return castc[T, V]{c}
+	return cast[T, V]{c}
 }
