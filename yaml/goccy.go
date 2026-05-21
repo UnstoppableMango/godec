efe3d60246961ff339da8b082e2b942d2c493364
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-05-21 16:54:24 -0400

    Yaml ser/des options (#17)
    * Yaml ser/des options
    
    * ope

--- yaml/goccy.go
+++ yaml/goccy.go
@@ -10,7 +10,10 @@ import (
 
 var Goccy = NewGoccy[any]()
 
-type goccy[T any] struct{}
+type goccy[T any] struct {
+	dec []yaml.DecodeOption
+	enc []yaml.EncodeOption
+}
 
 func NewGoccy[T any]() goccy[T] {
 	return goccy[T]{}
@@ -20,12 +23,16 @@ func (goccy[T]) Name() string {
 	return "goccy/go-yaml"
 }
 
-func (goccy[T]) NewDecoder(r io.Reader) codec.Decoder[T] {
-	return internal.AnyD[T](yaml.NewDecoder(r))
+func (c goccy[T]) NewDecoder(r io.Reader) codec.Decoder[T] {
+	return internal.AnyD[T](
+		yaml.NewDecoder(r, c.dec...),
+	)
 }
 
-func (goccy[T]) NewEncoder(w io.Writer) codec.Encoder[T] {
-	return internal.AnyE[T](yaml.NewEncoder(w))
+func (c goccy[T]) NewEncoder(w io.Writer) codec.Encoder[T] {
+	return internal.AnyE[T](
+		yaml.NewEncoder(w, c.enc...),
+	)
 }
 
 func (goccy[T]) Marshal(v T) ([]byte, error) {
@@ -34,4 +41,18 @@ func (goccy[T]) Marshal(v T) ([]byte, error) {
 
 func (goccy[T]) Unmarshal(data []byte, v T) error {
 	return yaml.Unmarshal(data, v)
+}
+
+func (c goccy[T]) WithDecode(options ...yaml.DecodeOption) goccy[T] {
+	return goccy[T]{
+		dec: append(c.dec, options...),
+		enc: c.enc,
+	}
+}
+
+func (c goccy[T]) WithEncode(options ...yaml.EncodeOption) goccy[T] {
+	return goccy[T]{
+		dec: c.dec,
+		enc: append(c.enc, options...),
+	}
 }
